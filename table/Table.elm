02a42ebae2440f8712112cb3d8d27f1f9171c9b4
Adam Stuller <adam.stuller@protonmail.com>
2020-05-22 13:21:04 +0200

    insert added

--- table/Table.elm
+++ table/Table.elm
@@ -1,7 +1,10 @@
 import Html exposing (text)
 main = text
     <| Debug.toString
-    <| empty "margaretina_tabulka" [({ name = "marosova_tabulka" },[("key", "value")]), ({ name = "adamova_tabulka" },[("key", "value")])]
+    <| empty "adamova_tabulka"
+    <| insert "marosova_tabulka" "mama" "maros" 
+    <| insert "marosova_tabulka" "mama" "ema" 
+    <| [({ name = "marosova_tabulka" },[("key", "value")]), ({ name = "adamova_tabulka" },[("key", "value")])]
 
 
 
@@ -22,10 +25,33 @@ type alias Memory = List Table
 
 -- Functions for data type
 
+-- Checks if given [table] has given [name]
 is_table: String -> Table -> Bool
 is_table name table =
     (Tuple.first table).name == name 
 
+-- Returns header from table
+get_header: Table -> Header
+get_header table =
+    Tuple.first table
+
+-- Returns items from table
+get_items: Table -> List Item
+get_items table =
+    Tuple.second table
+
+-- Takes [name] of searched for [table] and whole memory, returns [table] if there or Nothing if not
+get_table: String -> Memory -> Maybe Table
+get_table name memory =
+    case memory of 
+        [] -> Nothing
+        first :: rest -> 
+            if is_table name first
+            then Just first
+            else get_table name rest   
+
+
+------------------------- Empty ---------------------------------------------------
 -- Takes name of table, memory with tables and returns new memory, with empty table with [name]
 -- If table is present, new empty table replaces it. If not present new table is added.
 empty: String -> Memory -> Memory
@@ -36,20 +62,32 @@ empty name memory =
             if is_table name first 
             then (Header name, []) :: rest
             else first :: empty name rest
+
+------------------------------------------------------------------------------------
     
+-------------------------- insert -----------------------------
+-- Helper function
+-- Inserts new entry to items in table, tables [items] and [key] with [value].
+-- Returns new list of items 
+insert_to_table:  String -> String -> List Item -> List Item
+insert_to_table  key value items =
+    case items of
+        [] -> [(key, value)] 
+        first :: rest -> 
+            if Tuple.first first == key
+            then (key, value) :: rest 
+            else first :: insert_to_table key value rest
 
--- get_table: String -> Memory -> Maybe Table
--- get_table name memory =
---     case memory of 
---         [] -> Nothing
---         first :: rest -> 
---             if (Tuple.first first).name == name     
+-- Inserts item into given memory, takes [name] of table, [key] nad [value] for entry and [memory]
+insert: String -> String -> String -> Memory -> Memory
+insert name key value memory =
+    case memory of 
+        [] -> []
+        first :: rest -> 
+            if is_table name first
+            then ( get_header first, insert_to_table key value (get_items first) ) :: rest
+            else first:: insert name key value rest
 
-
--- insert: String -> String -> String -> Table
--- insert name key value memory =
---     case memory of
---         [] -> []
---         first :: rest -> 
---             if (Tuple.first first).name == name 
+-------------------------------------------------------------------
+   
 
